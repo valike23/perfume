9cb79848d1646a3ae4ffa0537ece59c4ba0d6fe4
valike23 <ludike23@gmail.com>
2023-03-12 09:58:38 +0100

    update the images

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Bottom from '$lib/Bottom.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import Header from '$lib/Header.svelte';
+</script>
+
+<div class="hero_area pt-5 ">
+	<Header />
+</div>
+
+<Footer />
+<Bottom />
